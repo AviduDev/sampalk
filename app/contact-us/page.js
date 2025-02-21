72ccda516ea44a8226d6aa9e7a5f470abff9e98a
AviduDev <avidudev@gmail.com>
2025-02-21 22:11:06 +0530

    Testing Netlify Forms Again

--- app/contact-us/page.js
+++ app/contact-us/page.js
@@ -1,17 +1,52 @@
-import React from "react";
+'use client'
 
 export default function Contact() {
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  };
+
+  const handleSubmit = (e) => {
+    const data = {
+      name: e.target.name.value,
+      email: e.target.email.value,
+      message: e.target.message.value,
+    };
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact-form", ...data }),
+    })
+      .then(() => alert("Success!"))
+      .catch((error) => alert(error));
+
+    e.preventDefault();
+  };
+
+  const handleChange = (e) =>
+    this.setState({ [e.target.name]: e.target.value });
+
   return (
     <main>
-      <form name="contact" netlify>
+      <form name="contact-form" method="post" onSubmit={handleSubmit}>
+        <input type="hidden" name="form-name" value="contact" />
         <p>
           <label>
-            Name <input type="text" name="name" />
+            Your Name: <input type="text" name="name" />
           </label>
         </p>
         <p>
           <label>
-            Email <input type="email" name="email" />
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
           </label>
         </p>
         <p>
